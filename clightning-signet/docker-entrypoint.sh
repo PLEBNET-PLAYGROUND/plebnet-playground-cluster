26e9d4135ec598f55554979dbda28456da4eb91a
Richard Safier <richardj@safier.com>
2021-09-13 12:52:19 -0400

    almost there, env vars are not getting to bash script or I'm retarded, not sure which

--- clightning-signet/docker-entrypoint.sh
+++ clightning-signet/docker-entrypoint.sh
@@ -1,21 +1,28 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
+set -eo pipefail
+echo "connect = ${BITCOIN-RPCCONNECT}"
+initial_config_file()
+{
+  echo "
+bitcoin-rpcconnect=${BITCOIN-RPCCONNECT}
+bitcoin-rpcuser=${bitcoin-rpcuser}
+bitcoin-rpcpassword=${bitcoin-rpcpassword}
+alias=${alias}
+proxy=${proxy}
+log-file=${log-file}
+tor-service-password=${tor-service-password}
+" > /root/.lightning/signet/config
+} 
 
-# : "${EXPOSE_TCP:=false}"
+if [[ ! -f /root/.lightning/signet/config ]]; then
+  mkdir -p /root/.lightning/signet/
+  echo "signet/config file not found in volume, building."
+  initial_config_file
+else
+  echo "signet/config file exists, skipping."
+fi
 
 networkdatadir="${LIGHTNINGD_DATA}/${LIGHTNINGD_NETWORK}"
-
-# if [ "$EXPOSE_TCP" == "true" ]; then
-#     set -m
-#     lightningd "$@" &
-
-#     echo "C-Lightning starting"
-#     while read -r i; do if [ "$i" = "lightning-rpc" ]; then break; fi; done \
-#     < <(inotifywait -e create,open --format '%f' --quiet "${networkdatadir}" --monitor)
-#     echo "C-Lightning started"
-#     echo "C-Lightning started, RPC available on port $LIGHTNINGD_RPC_PORT"
-
-#     socat "TCP4-listen:$LIGHTNINGD_RPC_PORT,fork,reuseaddr" "UNIX-CONNECT:${networkdatadir}/lightning-rpc" &
-#     fg %-
-# else
-    exec $@
-# fi
+ 
+exec $@
+ 
