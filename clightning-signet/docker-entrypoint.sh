abb91a0294c48f1c4ec1e8c2533def3c5d55058d
Richard Safier <richardj@safier.com>
2021-09-13 14:20:42 -0400

    don't use dashes use underscores

--- clightning-signet/docker-entrypoint.sh
+++ clightning-signet/docker-entrypoint.sh
@@ -1,21 +1,22 @@
-#!/usr/bin/env bash 
+#!/bin/bash 
 set -eo pipefail
-echo "connect = ${BITCOIN-RPCCONNECT}"
+
 initial_config_file()
 {
-  echo "
-bitcoin-rpcconnect=${BITCOIN-RPCCONNECT}
-bitcoin-rpcuser=${bitcoin-rpcuser}
-bitcoin-rpcpassword=${bitcoin-rpcpassword}
+ echo "bitcoin-rpcconnect=${BITCOIN_RPCCONNECT}
+bitcoin-rpcuser=${bitcoin_rpcuser}
+bitcoin-rpcpassword=${bitcoin_rpcpassword}
 alias=${alias}
 proxy=${proxy}
-log-file=${log-file}
-tor-service-password=${tor-service-password}
-" > /root/.lightning/signet/config
+log-file=${log_file}
+tor-service-password=${tor_service_password}" > /root/.lightning/signet/config
 } 
 
+ mkdir -p /root/.lightning/signet/
+
+
+
 if [[ ! -f /root/.lightning/signet/config ]]; then
-  mkdir -p /root/.lightning/signet/
   echo "signet/config file not found in volume, building."
   initial_config_file
 else
