d77da09a4c70313762528e0aa643bc5f20c0dc24
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-05-20 15:27:46 -0400

    up-generic.sh: handle ./up-generic.sh

--- up-generic.sh
+++ up-generic.sh
@@ -1,13 +1,16 @@
 #This is for internal testing only
+if [ -z "$1" ]; then
+    $1=5
+fi
 : ${TRIPLET:=x86_64-linux-gnu}
-: ${bitcoind=1}
-: ${lnd=1}
-: ${tor=1}
+: ${bitcoind=$1}
+: ${lnd=$1}
+: ${tor=$1}
 
 python plebnet_generate.py TRIPLET=$TRIPLET bitcoind=$bitcoind lnd=$lnd tor=$tor
 
 #Remove
-docker-compose down 
+docker-compose down
 sudo rm -rf volumes
 
 
@@ -16,17 +19,17 @@ sudo rm -rf volumes
 mkdir volumes
 for (( i=0; i<=$bitcoind-1; i++ ))
 do
-    mkdir volumes/bitcoin_datadir_$i
+    mkdir -p volumes/bitcoin_datadir_$i
 done
 for (( i=0; i<=$lnd-1; i++ ))
 do
-    mkdir volumes/lnd_datadir_$i
+    mkdir -p volumes/lnd_datadir_$i
 done
 for (( i=0; i<=$tor-1; i++ ))
 do
-    mkdir volumes/tor_datadir_$i
-    mkdir volumes/tor_servicesdir_$i
-    mkdir volumes/tor_torrcdir_$i
+    mkdir -p volumes/tor_datadir_$i
+    mkdir -p volumes/tor_servicesdir_$i
+    mkdir -p volumes/tor_torrcdir_$i
 done
 
 docker-compose build --build-arg TRIPLET=$TRIPLET
